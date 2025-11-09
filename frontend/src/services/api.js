15d0586fec16a8edbe78c2b735ab040c85e790dd
”lavanyanarada” <lavanyanarada22@gmail.com>
2025-11-09 15:31:01 +0530

    frontend commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const BASE_URL ="https://ai-wiki-quiz-generator-j8j0.onrender.com";
 
 export async function generateQuiz(url) {
   const resp = await fetch(`${BASE_URL}/generate_quiz`, {
